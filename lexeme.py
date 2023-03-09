d4b58e1bce67d531828ffb2450177ac52654c27f
safepre <safelgpreciado@gmail.com>
2023-03-08 20:15:04 -0800

    Version 2.5

--- lexeme.py
+++ lexeme.py
@@ -15,49 +15,79 @@ logical_Operators = [ "&&", "||", "!" ]
 punctuations = [",", ".", ";", "\"", "'", "(", ")", "{", "}", "[", "]", "=" ]
 
 filename = input('Please enter a Java file name: ')
-counterKey, counterArithmetic, counterComparison, counterLogical, counterPunctuations = 0, 0, 0, 0, 0
-no_Duplicates = set()
+counterKey, counterArithmetic, counterComparison, counterLogical, counterPunctuations, counterDouble, counterInteger, counterOther = 0, 0, 0, 0, 0,0,0,0
+keywordSet, logicalOpSet, arithmeticOpSet, comparisonOpSet, punctuationSet, integerLiteralsSet, doubleLiteralsSet, otherSet = set(),set(),set(),set(),set(),set(),set(),set()
 fileType = filename.split(".").pop()
-
 if fileType == 'java':
     with open(filename, "r") as file:
         file_data = file.read()
-        lexemes = re.findall(r"\w+|[^\w\s]+|!=|<=|>=|\|\||&&|==", file_data) 
+        lexemes = re.findall(r'\w+|[^\w\s]+|!=|<=|>=|\|\||&&|==', file_data) 
         for lexeme in lexemes:
             if lexeme in keywords:
-                counterKey += 1
-                if lexeme not in no_Duplicates:
-                    no_Duplicates.add(lexeme)
-                    print('Keyword: ' + lexeme)
-            if lexeme in arithmetic_Operators:
-                counterArithmetic += 1
-                if lexeme not in no_Duplicates:
-                    no_Duplicates.add(lexeme)
-                    print('Arithmetic Operator: ' + lexeme)
-            if lexeme in comparison_Operators:
-                counterComparison += 1
-                if lexeme not in no_Duplicates:
-                    no_Duplicates.add(lexeme)
-                    print('Comparison Operator: ' + lexeme)
-            if lexeme in logical_Operators:
-                counterLogical += 1
-                if lexeme not in no_Duplicates:
-                    no_Duplicates.add(lexeme)
-                    print('Logical Operator: ' + lexeme)
-            if lexeme in punctuations:
-                counterPunctuations += 1
-                if lexeme not in no_Duplicates:
-                    no_Duplicates.add(lexeme)
-                    print('Punctuation: ' + lexeme)
+                if lexeme not in keywordSet:
+                    counterKey += 1
+                    keywordSet.add(lexeme)
+            elif lexeme in arithmetic_Operators:
+                if lexeme not in arithmeticOpSet:
+                    counterArithmetic += 1
+                    arithmeticOpSet.add(lexeme)
+            elif lexeme in comparison_Operators:
+                if lexeme not in comparisonOpSet:
+                    counterComparison += 1
+                    comparisonOpSet.add(lexeme)
+            elif lexeme in logical_Operators:
+                if lexeme not in logicalOpSet:
+                    counterLogical += 1
+                    logicalOpSet.add(lexeme)
+            elif lexeme in punctuations:
+                if lexeme not in punctuationSet:
+                    counterPunctuations += 1
+                    punctuationSet.add(lexeme)
+            elif re.match(r'\d+',lexeme):
+                if punctuations[1] in lexeme:
+                    if lexeme not in doubleLiteralsSet:
+                        counterDouble += 1
+                        doubleLiteralsSet.add(lexeme)
+                else:
+                    if lexeme not in integerLiteralsSet:
+                        counterInteger += 1
+                        integerLiteralsSet.add(lexeme)
+            else:
+                if lexeme not in otherSet:
+                    counterOther += 1
+                    otherSet.add(lexeme)
+        
         if counterKey == 0:
-            print('Keyword is { } ')
+            print('Keywords: { } ')
+        else:
+            print('Keywords: ', counterKey,' : ',keywordSet)
         if counterArithmetic == 0:
-            print('Arithmetic Operator is { } ')
+            print('Arithmetic Operators: { } ')
+        else:
+            print('Arithmetic Operators: ', counterArithmetic,' : ',arithmeticOpSet)
         if counterComparison == 0:
-            print('Comparison Operator is { } ')
+            print('Comparison Operators: { } ')
+        else:
+            print('Comparison Operators: ', counterComparison, ' : ', comparisonOpSet)
         if counterLogical == 0:
-            print('Logical Operator is { } ')
+            print('Logical Operators: { } ')
+        else:
+            print('Logical Operators: ', counterLogical, ' : ', logicalOpSet)
         if counterPunctuations == 0:
-            print('Punctuations is { } ')
+            print('Punctuations: { } ')
+        else:
+            print('Punctuations: ', counterPunctuations, ' : ', punctuationSet)
+        if counterDouble == 0:
+            print('Double Literals: { }')
+        else:
+             print('Double Literals: ', counterDouble, ' : ', doubleLiteralsSet)
+        if counterInteger == 0:
+            print('Integer Literals: { }')
+        else:
+            print('Integer Literals: ', counterInteger, ' : ', integerLiteralsSet)
+        if counterOther == 0:
+            print('Other Lexemes: { }')
+        else:
+            print('Other Lexemes: ', counterOther, ' : ', otherSet)
 else:
     print("Wrong file type.")
